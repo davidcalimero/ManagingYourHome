c7251ff55c89aca9f3e95e5a0e5a30552c5f5000
Vânia Mendonça <vania.pp.mendonca@gmail.com>
2013-12-26 15:42:25 +0000

    Planta

---
+++ php/planta.php
@@ -0,0 +1,35 @@
+<!-- *********************************************************************** -->
+<!-- ************************ Managing Your Home *************************** -->
+<!-- *********************************************************************** -->
+
+
+<!-- **************************** Grupo 302 ******************************** -->
+
+
+<html>
+	<head>
+
+		<!-- Propriedades do documento -->
+		<meta content="text/html; charset=UTF-8" http-equiv="content-type"/>
+
+		<!-- CSS -->
+		<link rel="stylesheet" type="text/css" href="../stylesheet.css">
+
+		<!-- JQuery -->
+		<script type="text/javascript" src="../scripts/jquery.js"></script>
+
+		<title>Managing Your Home</title>
+	</head>
+	<body>
+
+		<table width="100%" height="100%">
+			<tr><td id="container">
+				
+			</td></tr>
+		</table>
+
+		<!-- JavaScripts -->
+		<script type="text/javascript" src="../scripts/scripts.js"></script>
+
+	</body>
+</html>
