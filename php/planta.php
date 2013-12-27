4402e77a4e1285173bad42c9bac94dffc3ef59aa
Vânia Mendonça <vania.pp.mendonca@gmail.com>
2013-12-27 12:51:08 +0000

    Planta in progress

--- php/planta.php
+++ php/planta.php
@@ -18,18 +18,33 @@
 		<!-- JQuery -->
 		<script type="text/javascript" src="../scripts/jquery.js"></script>
 
+		<!-- JavaScripts -->
+		<script type="text/javascript" src="../scripts/scripts.js"></script>
+
 		<title>Managing Your Home</title>
 	</head>
 	<body>
-
 		<table width="100%" height="100%">
 			<tr><td id="container">
-				
+				<table id="layout" border="1">
+					<tr>
+						<td id="sidebar">Eu sou uma sidebar</td>
+						<td id="main">
+						<!-- ******************* -->
+							<div id="submain"> 
+								<div class="item">	
+									<img src="../media/img/cama.gif"/>	
+								</div> 
+							 	<div class="itemContainer">
+									<img src="../media/img/Planta.png"/> 
+								</div>
+							</div>
+						<!-- ******************* -->
+						</td>
+					</tr>
+				</table>
 			</td></tr>
-		</table>
-
-		<!-- JavaScripts -->
-		<script type="text/javascript" src="../scripts/scripts.js"></script>
+		</table>		
 
 	</body>
 </html>
