a6345bfb08de5bf8c26e9f3555b91fb8ed6e81dd
Vânia Mendonça <vania.pp.mendonca@gmail.com>
2013-12-29 09:29:14 +0000

    Settings in progress

---
+++ php/definicoes.php
@@ -0,0 +1,62 @@
+<!-- *********************************************************************** -->
+<!-- ************************ Managing Your Home *************************** -->
+<!-- *********************************************************************** -->
+
+
+<!-- **************************** Grupo 302 ******************************** -->
+
+
+<html>
+	<head>
+
+		<!-- Propriedades do documento -->
+		<meta content="text/html; charset=UTF-8" http-equiv="content-type"/>
+
+		<!-- CSS -->
+		<link rel="stylesheet" type="text/css" href="../stylesheet.css">
+
+		<!-- JQuery -->
+		<script type="text/javascript" src="../scripts/jquery.js"></script>
+
+		<title>Managing Your Home</title>
+	</head>
+	<body>
+
+		<table width="100%" height="100%">
+			<tr><td id="container">
+				<h1 id="settingsTitle">Definições</h1>
+
+				<form name="settingsForm">
+						<table id="settings">
+							<tr>
+								<td class="settingsLabel">Volume:</td>
+								<td><!-- <input type="text" name="username" id="usernameInput" class="loginFields"/> --></td>
+							</tr>
+							<tr>
+								<td class="settingsLabel">Modo de acessibilidade:</td>
+								<td><!--<input type="password" name="password" id="passwordInput" class="loginFields"/>--></td>
+							</tr>
+
+							<tr>
+								<td class="settingsLabel">Idioma:</td>
+								<td> <!-- <input type="button" name="criarConta" value="Criar conta" id="createAccount" class="loginButtons"/> --> </td>
+							</tr>
+
+							<tr><td colspan="2" class="settingsButtons">
+								<!-- <input type="button" name="iniciarSessao" value="Iniciar sessão" id="loginButton" class="loginButtons"/> 
+								<input type="reset" name="cancelar" value="Cancelar" id="cancelButton" class="loginButtons"/> -->
+							</td></tr>
+						</table>
+					</form>
+				<!--
+				<img src="../media/img/logo.png" id="logo"/>		
+				&nbsp;
+				-->
+			</td></tr>
+		</table>
+
+		<!-- JavaScripts -->
+		<script type="text/javascript" src="../scripts/scripts.js"></script>
+
+	</body>
+</html>
